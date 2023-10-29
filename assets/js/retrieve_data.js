5d6d01aa428ce77db77f7668019ace857ed090b9
Vincent <vjc9@duke.edu>
2023-10-28 22:54:34 -0400

    more fully fleshed product page

--- assets/js/retrieve_data.js
+++ assets/js/retrieve_data.js
@@ -1,16 +1,27 @@
-// Access the product data passed from HTML
-const data = productData;
+async function fetchData(filepath) {
+    try {
+        const response = await fetch(filepath);
+        const data = await response.json();
 
-// Set product image
-document.getElementById('product-image').src = data.image;
+        // Set product title
+        document.querySelector('.section-title h2').textContent = data.title;
 
-// Create a simple map for specifications
-const specifications = data.specifications;
-let specHTML = '<h2>Specifications</h2><ul>';
-for (const spec in specifications) {
-    specHTML += `<li><strong>${spec}:</strong> ${specifications[spec]}</li>`;
+        // Set product image
+        document.getElementById('product-image').src = data.image;
+
+        // Populate specifications dynamically
+        const specifications = data.specifications;
+        let specHTML = '<h7><table border="1" width="650" height="550" class="text-center"><caption></caption><thead><tr><th>Property</th><th>Value</th></tr></thead><tbody>';
+
+        for (const spec in specifications) {
+            specHTML += `<tr><td>${spec}</td><td>${specifications[spec]}</td></tr>`;
+        }
+
+        specHTML += '</tbody></table></h7>';
+
+        // Display specifications
+        document.getElementById('spec-details').innerHTML = specHTML;
+    } catch (error) {
+        console.error('Error fetching or displaying product data:', error);
+    }
 }
-specHTML += '</ul>';
-
-// Display specifications
-document.getElementById('spec-details').innerHTML = specHTML;
